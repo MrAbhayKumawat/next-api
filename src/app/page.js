ea5e8531a847fb37b7bc05f18608161e773a3f46
Abhay kumawat <abhaykumawat81@gmail.com>
2024-03-07 09:19:50 +0530

    canges in home  page

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-   <Link href={"/api/products"}>Open Product API</Link>
+      <Link href={"/api/products"}>Open Product API</Link>
+      <h1>HEllo Workd </h1>
     </main>
   );
 }
